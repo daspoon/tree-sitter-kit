9bf9c02c891e6eac3e505eb5ae6dfcff96ca182e
David Spooner <daspoon@me.com>
2024-03-23 08:35:53 -0600

    Revert to use of underlying TSCursor.

--- Sources/TreeSitterKit/TSCursor.swift
+++ Sources/TreeSitterKit/TSCursor.swift
@@ -7,153 +7,87 @@
 import TreeSitter
 
 
-/// A variation on *TSTreeCursor* which provides scanner-like functionality.
-public struct TSCursor
+/// A minimal variation on *TSTreeCursor* which provides scanner-like functionality. This interface will be extended with hierarchy navigation as necessary.
+public class TSCursor
   {
-    // Note: An initial implementation via TSTreeCursor did not behave as expected, so an explict stack was used instead...
+    private var _cursor : TSTreeCursor
+    private var _node : TSNode!
 
-    private let tree : TSTree
-    private var stack : [State]
-    struct State {
-      let parent : TSNode
-      let count : Int
-      var index : Int
-      init(parent p: TSNode) { parent = p; count = p.count; index = 0 }
-      var node : TSNode? { index < count ? parent[index] : nil }
+    /// Initialize a new instance pointing to the given node.
+    public init(node: TSNode) {
+      _cursor = ts_tree_cursor_new(node)
+      _node = node
     }
 
-    /// Initialize a new instance pointing to the *i*-th subnode of the given node; fails if either the given node has no subnodes or the given index is out of range.
-    public init(tree t: TSTree) {
-      tree = t
-      stack = [.init(parent: t.rootNode)]
+    /// Initialize a new instance pointing to the first subnode of the given node; fails if the given node has no subnodes.
+    public convenience init(subnodesOf parent: TSNode) throws {
+      guard parent.count > 0
+        else { throw TSException("") }
+      self.init(node: parent[0])
+    }
+
+    deinit {
+      ts_tree_cursor_delete(&_cursor)
     }
 
     /// The current node, or *nil* if there is none.
-    public var node : TSNode! {
-      stack.last?.node
-    }
+    public var node : TSNode!
+      { _node }
 
-    /// Advance and return the current node if it is non-*nil* and has the given type, otherwise throw.
+    /// Advance and return *true* iff the current node is non-*nil* and has the given type.
     @discardableResult
-    public mutating func scanNode(of type: String? = nil) throws -> TSNode {
-      guard let node
-        else { throw TSException("missing node\(type.map {" of type '\($0)'"} ?? "")") }
-      guard type == nil || type! == node.type
-        else { throw TSException("expecting '\(type!)' node; found \(node.type)") }
-      guard node.isError == false
-        else { throw TSException("encounered error node\(type.map {" of type '\($0)'"} ?? "")") }
-      stack[stack.count-1].index += 1
-      return node
+    public func scanNode(of type: String? = nil) -> Bool {
+      guard let node, type == nil || type == .some(node.type)
+        else { return false }
+      let didAdvance = ts_tree_cursor_goto_next_sibling(&_cursor)
+      _node = didAdvance ? ts_tree_cursor_current_node(&_cursor) : nil
+      return true
     }
 
-    /// Scan and transform the current node.
-    public mutating func scanNode<T>(of type: String? = nil, transform: (TSNode) throws -> T) throws -> T {
-      try transform(try scanNode(of: type))
-    }
-
-    /// A convenience type for specifying a bracket symbol pairs.
-    public struct BracketPair {
-      public let leading : String
-      public let trailing : String
-      public init(leading l: String, trailing t: String) { leading = l; trailing = t }
-      public static var angle : Self { .init(leading: "<", trailing: ">") }
-      public static var curly : Self { .init(leading: "{", trailing: "}") }
-      public static var round : Self { .init(leading: "(", trailing: ")") }
-      public static var square : Self { .init(leading: "[", trailing: "]") }
-    }
-
-    /// Scan a sequence of elements delimited by the given separator type and optionally enclosed by the given bracket types. The method (which takes the receiver and the index of the element to scan) must either advance the receiver's state and return *true* if an element is recognized, or leave the state intact and return *false* otherwise. Returns the number of scanned elements.
+    /// Scan a sequence of elements delimited by the given separator type and optionally enclosed by the given bracket types. The given method (which is given the receiver and the index of the element to be scanned) must either advance the receiver's state and return *true* if an element is recognized, or leave the state intact and return *false* otherwise. Returns the number of scanned elements.
     @discardableResult
-    public mutating func scanElements(bracketedBy brackets: BracketPair? = nil, separatedBy separator: String, using process: (inout TSCursor, Int) throws -> Bool) throws -> Int {
+    public func scanElements(bracketedBy brackets: (leading: String, trailing: String)?, separatedBy separator: String, using process: (TSCursor, Int) throws -> Void) throws -> Int {
       var count : Int = 0
-      if let brackets {
-        try scanNode(of: brackets.leading)
-      }
+      guard brackets.map({scanNode(of: $0.leading)}) ?? true
+        else { throw TSException("expecting leading '\(brackets!.leading)'") }
       while let node, brackets?.trailing != .some(node.type) {
-        if count > 0 {
-          try scanNode(of: separator)
-        }
-        if try process(&self, count) == false {
-          break
-        }
+        guard count == 0 || scanNode(of: separator)
+          else { throw TSException("expecting '\(separator)' separator") }
+        try process(self, count)
         count += 1
       }
-      if let brackets {
-        try scanNode(of: brackets.trailing)
-      }
+      guard brackets.map({scanNode(of: $0.trailing)}) ?? true
+        else { throw TSException("expecting trailing '\(brackets!.trailing)'") }
       return count
     }
 
-    /// Scan a sequence of elements delimited by the given separator type and optionally enclosed by the given bracket types. The method (which takes the receiver and the index of the element to scan) must either advance the receiver's state and return an element value, or leave the state intact and return *nil*. Returns the array of scanned elements.
-    public mutating func scanArray<T>(bracketedBy brackets: BracketPair? = nil, separatedBy separator: String, using process: (inout TSCursor, Int) throws -> T?) throws -> [T] {
+    /// Scan a sequence of elements delimited by the given separator type and optionally enclosed by the given bracket types. The given method (which is given the receiver and the index of the element to be scanned) must either advance the receiver's state and return the scanned element, or leave the state intact and return *nil*. Returns the array of scanned elements.
+    public func scanElements<T>(of type: T.Type = T.self, bracketedBy brackets: (leading: String, trailing: String)?, separatedBy separator: String, using process: (TSCursor, Int) throws -> T) throws -> [T] {
       var elements : [T] = []
-      _ = try scanElements(bracketedBy: brackets, separatedBy: separator) { cursor, index in
-        guard let element = try process(&cursor, index)
-          else { return false }
-        elements.append(element)
-        return true
+      _ = try scanElements(bracketedBy: brackets, separatedBy: separator) { scanner, index in
+        elements.append(try process(self, index))
       }
       return elements
     }
 
-    public mutating func scanKeyValuePairs<K, V>(bracketedBy brackets: BracketPair, separatedBy separator: String, using process: (inout TSCursor, Int) throws -> (K, V)?) throws -> ([K], [V]) where K: Hashable {
-      var pairs : ([K], [V]) = ([], [])
-      var keys : Set<K> = []
-      _ = try scanElements(bracketedBy: brackets, separatedBy: separator) { cursor, index in
-        guard let pair = try process(&cursor, index)
-          else { return false }
-        guard keys.contains(pair.0) == false
-          else { throw TSException("duplicate key: '\(pair.0)'") }
-        keys.insert(pair.0)
-        pairs.0.append(pair.0)
-        pairs.1.append(pair.1)
-        return true
-      }
-      return pairs
+    /// Descend into the current node. Throw if the current node is nil or has no subnodes.
+    public func enter() throws -> TSNode {
+      guard let node
+        else { throw TSException("node is nil") }
+      guard node.count > 0
+        else { throw TSException("node is a leaf") }
+      ts_tree_cursor_goto_first_child(&_cursor)
+      _node = ts_tree_cursor_current_node(&_cursor)
+      return _node
     }
 
-    /// Returns the result of invoking the preceding method if the current node represents a leading bracket, or *[]* otherwise.
-    public mutating func scanOptionalArray<T>(bracketedBy brackets: BracketPair, separatedBy separator: String, using process: (inout TSCursor, Int) throws -> T?) throws -> [T] {
-      guard node?.type == brackets.leading
-        else { return [] }
-      return try scanArray(bracketedBy: brackets, separatedBy: separator, using: process)
-    }
-
-    /// Scan a sequence of elements delimited by the given separator type and optionally enclosed by the given bracket types. The method (which takes the receiver and the index of the element to scan) must either advance the receiver's state and return a key/value pair, or leave the state intact and return *nil*. Returns the dictionary of scanned key/value pairs.
-    public mutating func scanDictionary<K, V>(bracketedBy brackets: BracketPair?, separatedBy separator: String, using process: (inout TSCursor, Int) throws -> (K, V)?) throws -> [K: V] {
-      try Dictionary(try scanArray(bracketedBy: brackets, separatedBy: separator, using: process)) { _, _ in
-        throw TSException("duplicate keys")
-      }
-    }
-    /// Returns the result of invoking the preceding method if the current node represents a leading bracket, or *[:]* otherwise.
-    public mutating func scanOptionalDictionary<K, V>(bracketedBy brackets: BracketPair, separatedBy separator: String, using process: (inout TSCursor, Int) throws -> (K, V)?) throws -> [K: V] {
-      guard node?.type == brackets.leading
-        else { return [:] }
-      return try scanDictionary(bracketedBy: brackets, separatedBy: separator, using: process)
-    }
-
-    /// Descend into the current node and return the *type* of that node. Return *nil* if the current node is *nil* or has no subnodes.
-    public mutating func enter() -> String? {
-      guard let node, node.count > 0
-        else { return nil }
-      stack.append(.init(parent: node))
-      return node.type
-    }
-
-    /// Ascend out of the current node, optinoally advancing to the next sibling of the current node's parent (as the default behavior). Each invocation of this method must balance a prior invocation of *enter*.
-    public mutating func exit(advance: Bool = true) {
-      precondition(stack.count > 1)
-      stack.removeLast()
-      if advance {
-        stack[stack.count-1].index += 1
-      }
-    }
-  }
-
-
-extension TSCursor : CustomStringConvertible
-  {
-    public var description : String {
-      "(stack: [\(stack.map({"(\($0.parent.type), \($0.index)<\($0.count))"}).joined(separator: ", "))], node: \(node?.type ?? "nil"))"
+    /// Ascend out of the current node. Throw if the current node is nil or has no parent.
+    public func exit() throws -> TSNode {
+      guard let node
+        else { throw TSException("node is nil") }
+      guard ts_tree_cursor_goto_parent(&_cursor)
+        else { throw TSException("node has no parent") }
+      _node = ts_tree_cursor_current_node(&_cursor)
+      return _node
     }
   }
