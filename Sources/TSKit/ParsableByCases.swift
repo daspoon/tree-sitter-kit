a48a08142d1260cf44ef6a800e5240c652776fd0
David Spooner <daspoon@me.com>
2024-04-08 09:33:01 -0600

    Improve inline documentation.

--- Sources/TSKit/ParsableByCases.swift
+++ Sources/TSKit/ParsableByCases.swift
@@ -5,13 +5,14 @@
 */
 
 
-/// *ParsableByCases* simplifies conformance to *Parsable* for types which require multiple production rules, such as enums.
+/// *ParsableByCases* simplifies conformance to *Parsable* for types which require multiple production rules, such as enums. It provides a default implementation for *syntaxExpression* as a choice of sub-rule productions.
 public protocol ParsableByCases : Parsable {
-  /// Return the mapping of rule names to syntax expressions.
+  /// Return the mapping of sub-rule names to syntax expressions.
   static var syntaxExpressionsByCaseName : [String: TSExpression] { get }
 }
 
 extension ParsableByCases {
+  /// Return a choice of the productions for the sub-rules defined by *syntaxExpressionsByCaseName*.
   public static var syntaxExpression : TSExpression {
     .choice(syntaxExpressionsByCaseName.keys.sorted().map { .prod(ProductionRule(for: $0, of: Self.self)) })
   }
