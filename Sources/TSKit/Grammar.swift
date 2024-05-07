a48c017d11ace66731f53e30830d9f00da919751
David Spooner <daspoon@me.com>
2024-05-07 11:18:41 -0600

    checkpoint: success?

--- Sources/TSKit/Grammar.swift
+++ Sources/TSKit/Grammar.swift
@@ -12,22 +12,22 @@ public struct Grammar {
   /// The name assigned to the generated grammar.
   public let name : String
   /// The production rule for the root type.
-  public let rootRule : AnyProductionRule
+  public let rootSymbol : Symbol
   /// Optionally specify the grammar's word rule.
-  public let wordRule : AnyProductionRule?
+  public let wordSymbol : Symbol?
 
-  public init<T: Parsable>(name n: String, rootType t: T.Type) {
+  public init<Root: Parsable>(name n: String, rootType: Root.Type) {
     name = n
-    rootRule = T.productionRule.typeErased
-    wordRule = nil
+    rootSymbol = .init(rootType)
+    wordSymbol = nil
   }
 
   public init<Root: Parsable, Word: Parsable>(name n: String, rootType: Root.Type, wordType: Word.Type) {
     guard case .pattern = wordType.productionRule.syntaxExpression
       else { preconditionFailure("\(Word.self).syntaxExpression must be a pattern") }
     name = n
-    rootRule = Root.productionRule.typeErased
-    wordRule = Word.productionRule.typeErased
+    rootSymbol = .init(rootType)
+    wordSymbol = .init(wordType)
   }
 
   /// Return the symbol for the start rule for any instance.
@@ -35,53 +35,51 @@ public struct Grammar {
     "start"
   }
 
-  /// Return the set of rules reachable from the receiver, excluding the receiver itself.
-  private var supportingRules : [AnyProductionRule] {
-    var accumulatedRules : [AnyProductionRule] = []
+  /// Return the set of symbols reachable from the root symbol; include the 'word' rule,
+  /// but exclude the root symbol.
+  private var supportingSymbols : Set<Symbol> {
     var visitedSymbols : Set<Symbol> = []
-    var remainingSymbols : Set<Symbol> = [rootRule.symbol]
+    var remainingSymbols : Set<Symbol> = [rootSymbol]
+    func walk(_ expr: TSExpression) {
+      switch expr {
+        case .prod(let supportingSymbol) :
+          guard visitedSymbols.contains(supportingSymbol) == false else { break }
+          remainingSymbols.insert(supportingSymbol)
+        case .seq(let exprs), .choice(let exprs) :
+          exprs.forEach { walk($0) }
+        case .field(_, let expr), .optional(let expr), .prec(_, let expr), .repeat(let expr) :
+          walk(expr)
+        case .literal, .pattern :
+          break
+      }
+    }
+    var symbols : Set<Symbol> = []
     while remainingSymbols.isEmpty == false {
       let symbol = remainingSymbols.removeFirst()
       let rule = symbol.productionRule
       visitedSymbols.insert(symbol)
-      if symbol != rootRule.symbol {
-        accumulatedRules.append(rule)
-      }
-      func walk(_ expr: TSExpression) {
-        switch expr {
-          case .prod(let supportingSymbol) :
-            guard visitedSymbols.contains(supportingSymbol) == false else { break }
-            remainingSymbols.insert(supportingSymbol)
-          case .seq(let exprs), .choice(let exprs) :
-            exprs.forEach { walk($0) }
-          case .field(_, let expr), .optional(let expr), .prec(_, let expr), .repeat(let expr) :
-            walk(expr)
-          case .literal, .pattern :
-            break
-        }
+      if symbol != rootSymbol {
+        symbols.insert(symbol)
       }
       walk(rule.syntaxExpression)
     }
-    return accumulatedRules
+    return wordSymbol.map {symbols.union([$0])} ?? symbols
   }
 
   /// Return the javascript representation of a tree-sitter grammar.
   public var javascript : String {
-    // Form a list containing the root rule, each of its supporting rules sorted by ascending symbol name,
-    // and the word rule if specified and not among the supporting rules.
-    let supportingRules = self.supportingRules
-    var rules = [rootRule] + supportingRules.sorted(by: {$0.symbol.name < $1.symbol.name})
-    if let wordRule, supportingRules.contains(where: {$0.symbol == wordRule.symbol}) == false {
-      rules.append(wordRule)
-    }
+    // Form a list containing the root rule and each of the supporting rules (including
+    // the 'word') sorted by ascending symbol name.
+    let symbols = [rootSymbol] + supportingSymbols.sorted(by: {$0.name < $1.name})
+
     // Return the javascript source, with the start rule appearing first...
     return """
            module.exports = grammar({
                name: '\(name)',
-               \(wordRule.map { "word: $ => $.\($0.symbol.name)," } ?? "")
+               \(wordSymbol.map { "word: $ => $.\($0.name)," } ?? "")
                rules: {
-                   \(Self.startSymbol): $ => $.\(rootRule.symbol.name),
-                   \(rules.map({"\($0.symbol.name): $ => \($0.syntaxExpression.javascript)"})
+                   \(Self.startSymbol): $ => $.\(rootSymbol.name),
+                   \(symbols.map({"\($0.name): $ => \($0.productionRule.syntaxExpression.javascript)"})
                       .joined(separator: ",\n" + .space(8)))
                }
            })
