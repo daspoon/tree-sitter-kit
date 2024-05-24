1526e8039c5492852ed0bdcce549629d68cf182c
David Spooner <daspoon@me.com>
2024-05-24 13:33:26 -0600

    checkpoint

--- Sources/TSMacros/GrammarMacro.swift
+++ Sources/TSMacros/GrammarMacro.swift
@@ -10,87 +10,24 @@ import SwiftSyntaxMacros
 
 public struct GrammarMacro : MemberMacro {
   public static func expansion(of node: AttributeSyntax, providingMembersOf decl: some DeclGroupSyntax, in ctx: some MacroExpansionContext) throws -> [DeclSyntax] {
-    guard decl.is(StructDeclSyntax.self)
+    guard let decl = decl.as(StructDeclSyntax.self)
       else { throw Exception("unsupported declaration kind") }
 
-    // The grammar name is the name of the type of the affected declaration.
-    guard let name = decl.typeName
-      else { throw Exception("no type name") }
+    // Create a representative of the declaration.
+    let grammar = try GrammarRep(structDecl: decl)
 
     // Get the visibility of the affected declaration.
     let visibility = decl.visibility
 
-    // Get the alias for the Root type.
-    guard let rootType = decl.aliasType(for: "Root")
-      else { throw Exception("\(decl.typeName ?? "?") requires a typealias for Root") }
-
-    // Get the declaration of `static var productionRules : [ProductionRule]`
-    guard let binding = decl.variableBindingWith(name: "productionRules", type: "[ProductionRule]", isStatic: true)
-      else { throw Exception("requires implementation of 'static var productionRules : [ProductionRule]'") }
-    // Ensure it returns an array literal
-    guard let arrayExpr = binding.resultExpr?.as(ArrayExprSyntax.self)
-      else { throw Exception("productionRule must return an array literal") }
-    // Form a list of Rule instances from the list elements
-    let rules = try arrayExpr.elements
-      .map({try ProductionRuleImp($0.expression)})
-    // Form a map of type names to symbol names
-    let symbolNames = try Dictionary(rules.map {($0.typeName, $0.symbolName)}) {t,_ in throw Exception("multiple rules for '\(t)'")}
-    func symbolLookup(_ typeName: String) throws -> String {
-      guard let symbolName = symbolNames[typeName] else { throw Exception("no rule for type '\(typeName)'") }
-      return symbolName
-    }
-
-    // Get the symbol for the root type
-    let rootSymbol = try symbolLookup(rootType.text)
-
-    // Get the word symbol name from the declaration of `static var word : String?`, defaulting to nil.
-    let word : (symbol: String, pattern: String)?
-    if let wordBinding = decl.variableBindingWith(name: "word", type: "String?", isStatic: true) {
-      guard let pattern = wordBinding.resultExpr?.as(StringLiteralExprSyntax.self)?.stringLiteral
-        else { throw Exception("word must return a string literal") }
-      word = ("Word", pattern)
-    }
-    else {
-      word = nil
-    }
-
-    // Form the list of symbol names and syntax expression pairs from the defined rules, prepending
-    // an implicit start rule and optionally appending the Word rule.
-    let symbolNamesAndRawExpressions
-      = [("start", ProductionRuleImp.RawExpression.symbol(rootSymbol))]
-      + (try rules.flatMap({try $0.getSymbolNamesAndRawExpressions(symbolLookup)}))
-      + (word.map {[($0.symbol, ProductionRuleImp.RawExpression.pattern($0.pattern))]} ?? [])
-
-    // Create a json representation of the grammar; this has the consequence of ensuring that production rules
-    // exist for all referenced types.
-    let jsonText = """
-      \"\"\"
-      {
-        "name": "\(name)",
-        "rules": {
-          \(
-            symbolNamesAndRawExpressions
-              .map({"\"\($0)\": " + $1.json})
-              .joined(separator: ",\n    ")
-          )
-        },
-        \(word.map {"\"word\": \"\($0.symbol)\","} ?? "")
-        "extras": [\(ProductionRuleImp.RawExpression.whitespace.json)],
-        "conflicts": [],
-        "precedences": [],
-        "externals": [],
-        "inline": [],
-        "supertypes": []
-      }
-    \"\"\"
-    """
-
+    // Return the declarations for the generated methods...
     return [
-      DeclSyntax(stringLiteral: """
+      DeclSyntax(stringLiteral:
+        """
         \(visibility) static var jsonText : String {
-          \(jsonText)
+          \(grammar.jsonText)
         }
-        """),
+        """
+      ),
 //      DeclSyntax(stringLiteral: """
 //        \(visibility) static var language : TSLanguage {
 //          fatalError("todo")
@@ -98,19 +35,19 @@ public struct GrammarMacro : MemberMacro {
 //      """),
       DeclSyntax(stringLiteral: """
         \(visibility) static func translate(parseTree node: TSNode, in context: ParsingContext) -> Root {
-          extract\(rootType)(from: node, in: context)
+          extract\(grammar.rootType)(from: node, in: context)
         }
         """),
       DeclSyntax(stringLiteral: """
         \(visibility) static func isRuleHidden(for type: Any.Type) -> Bool {
           let hiddenTypes : Set<ObjectIdentifier> = [
-            \(rules.compactMap({$0.isSymbolHidden ? ".init(\($0.typeName).self)" : nil})
+            \(grammar.definedRules.compactMap({$0.isSymbolHidden ? ".init(\($0.typeName).self)" : nil})
               .joined(separator: ", "))
           ]
           return hiddenTypes.contains(ObjectIdentifier(type))
         }
         """),
-    ] + rules.map { rule in
+    ] + grammar.definedRules.map { rule in
       DeclSyntax(stringLiteral: rule.extractionDeclText)
     }
   }
---
+++ Sources/TSMacros/GrammarRep.swift
@@ -0,0 +1,85 @@
+/*
+
+  Created by David Spooner
+
+*/
+
+import SwiftSyntax
+
+
+struct GrammarRep {
+  // The type name of the declaration given on initialization.
+  let name : String
+  // The name of the 'Root' typealias, indicating the top-level language element.
+  let rootType : String
+  // The defined production rules.
+  let definedRules : [ProductionRuleImp]
+  // The symbol name and patter for the `word` attribute, if any.
+  let word : (symbol: String, pattern: String)?
+  // Return the complete list of production rules as pairs of symbols and syntax expressions.
+  let allRules : [(symbol: String, expression: ProductionRuleImp.RawExpression)]
+
+  // Initialize an instance from a struct declaration which conforms to the Grammar protocol.
+  init(structDecl decl: StructDeclSyntax) throws {
+    name = decl.name.text
+
+    // Get the name of the Root typealias.
+    rootType = try decl.aliasType(for: "Root")?.text
+      ?? { throw Exception("\(decl.typeName ?? "?") requires a typealias for `Root`") }()
+
+    // Get the pattern for the 'word' symbol, if any.
+    if let wordBinding = decl.variableBindingWith(name: "word", type: "String?", isStatic: true) {
+      guard let pattern = wordBinding.resultExpr?.as(StringLiteralExprSyntax.self)?.stringLiteral
+        else { throw Exception("`word` must return a string literal") }
+      word = ("Word", pattern)
+    }
+    else {
+      word = nil
+    }
+
+    // Get the list of production rules from `static var productionRules : [ProductionRule]`,
+    // which must be an array literal of calls to the ProductionRule init method.
+    guard let rulesBinding = decl.variableBindingWith(name: "productionRules", type: "[ProductionRule]", isStatic: true)
+      else { throw Exception("`productionRules` is required") }
+    guard let arrayExpr = rulesBinding.resultExpr?.as(ArrayExprSyntax.self)
+      else { throw Exception("`productionRules` must return an array literal") }
+    definedRules = try arrayExpr.elements.map({try ProductionRuleImp($0.expression)})
+
+    // Define a mapping of type names to symbol names for those production rules, along with
+    // a translation method which throws for undefined types.
+    let symbolsByType = try Dictionary(definedRules.map {($0.typeName, $0.symbolName)}) {t, _ in throw Exception("multiple rules for '\(t)'")}
+    func symbolLookup(_ typeName: String) throws -> String {
+      try symbolsByType[typeName] ?? { throw Exception("no rule for type '\(typeName)'") }()
+    }
+
+    allRules = [("start", ProductionRuleImp.RawExpression.symbol(try symbolLookup(rootType)))]
+      + (try definedRules.flatMap({try $0.getSymbolNamesAndRawExpressions(symbolLookup(_:))}))
+      + (word.map {[($0.symbol, ProductionRuleImp.RawExpression.pattern($0.pattern))]} ?? [])
+  }
+
+
+  /// Return the text of grammar.json.
+  var jsonText : String {
+    """
+    \"\"\"
+    {
+      "name": "\(name)",
+      "rules": {
+        \(
+          allRules
+            .map({"\"\($0)\": " + $1.json})
+            .joined(separator: ",\n    ")
+        )
+      },
+      \(word.map {"\"word\": \"\($0.symbol)\","} ?? "")
+      "extras": [\(ProductionRuleImp.RawExpression.whitespace.json)],
+      "conflicts": [],
+      "precedences": [],
+      "externals": [],
+      "inline": [],
+      "supertypes": []
+    }
+    \"\"\"
+    """
+  }
+}
