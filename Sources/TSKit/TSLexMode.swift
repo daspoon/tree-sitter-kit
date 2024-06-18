5a22e6d3c5379a694e8757f2ca907a10ed9e26f2
David Spooner <daspoon@me.com>
2024-06-18 10:55:11 -0600

    Add convenience constructors for TSLexMode

--- Sources/TSKit/TSLexMode.swift
+++ Sources/TSKit/TSLexMode.swift
@@ -11,4 +11,12 @@ extension TSLexMode {
   public init(lex_state s: UInt16) {
     self.init(lex_state: s, external_lex_state: 0)
   }
+
+  public static var zero : Self {
+    .init()
+  }
+
+  public static var endOfNonTerminalExtra : Self {
+    .init(lex_state: .max, external_lex_state: .max)
+  }
 }
